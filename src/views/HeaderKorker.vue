40ae03ac2be71ec1a2f93a71f95daa52248417d1
oskar <hej@hej.pl>
2026-07-09 13:39:07 +0200

    init router

--- src/views/HeaderKorker.vue
+++ src/views/HeaderKorker.vue
@@ -1,6 +1,5 @@
 <script setup></script>
 <template>
-<<<<<<< HEAD:src/views/HeaderKorker.vue
     <button>Korker</button>
     </template>
 <style scoped>
@@ -9,9 +8,3 @@ button {
     height: 100px;
 }
 </style>
-=======
-  <button>Korker</button>
-  <button>Czaty</button>
-</template>
-<style scoped></style>
->>>>>>> eebbfcd (podpowiedzi vscode):src/views/KorkerChat.vue
